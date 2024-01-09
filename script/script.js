41ed6fe6cbea4336af88c24f5a8ba910d0170102
ginterloper <ginterloper@gmail.com>
2024-01-09 13:08:12 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 tg = window.Telegram.WebApp;
 
-if (tg.platform == "unknown") location.replace("/platform");
+if (tg.platform == "unknown") window.location.replace("/platform");
 
 document.cookie = "huahinUser=" + tg.initDataUnsafe.user.username;
 document.cookie = "huahinUserID=" + tg.initDataUnsafe.user.id;
